d471971030e75fb5b1b53ecb988b6a70568a634f
ARK445 <limshealtiel@gmail.com>
2024-07-14 15:16:48 +0800

    test

---
+++ src/App.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        
+        
+        frame.setTitle("hello");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(420,420);
+        frame.setVisible(true);
+        
+
+
+    }
+}
