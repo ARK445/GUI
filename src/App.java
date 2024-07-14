c2d2b38f7d36fb37710e7f0478f7d6e49a00eb19
ARK445 <limshealtiel@gmail.com>
2024-07-14 17:57:46 +0800

    test

--- src/App.java
+++ src/App.java
@@ -1,18 +1,15 @@
-
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame();
-        
-        
-        frame.setTitle("hello");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(420,420);
-        frame.setVisible(true);
-        
-
-
+    public void Frame (){
+       
+        //JFrame frame = new JFrame();
+        //frame.setTitle("hello");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setResizable(false);
+        //frame.setSize(420,420);
+        //ImageIcon image = new ImageIcon("");
+         
     }
 }
---
+++ src/main.java
@@ -0,0 +1,10 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class main {
+    public static void main (String[] args){
+    App app = new App();
+    app.setVisible(true);
+
+    }
+}
